fd1986129b4e744147ec91e6bc43bde0ab574523
Sietse van der Molen <sietse@vdmolen.eu>
2013-01-28 02:01:33 +0200

    Fix tests to test new parser
    Signed-off-by: Sietse van der Molen <sietse@vdmolen.eu>

--- harness/tests/json_parser_test.adb
+++ harness/tests/json_parser_test.adb
@@ -1,87 +1,77 @@
 with AUnit.Assertions; use AUnit.Assertions;
-with jsonparser;
-with GNATCOLL.JSON;
+with JSONParser; use JSONParser;
+with AURParser; use AURParser;
 
 package body JSON_Parser_Test is
    procedure Test_NoJSONError (T : in out Test) is
-      use GNATCOLL.JSON;
-      pragma Unreferenced (T);
       jsonstring : constant String := "borked";
       json : JSON_Value;
+      pragma Unreferenced (T);
    begin
-      json := Read (Strm     => jsonstring,
-                    Filename => "");
-      jsonparser.ParseJSON (json => json);
+      json := ParseJSON (Strm     => jsonstring);
+      ParseAURResponse (json);
       Assert (False, "No Invalid_JSON_Stream exception raised");
    exception
-      when GNATCOLL.JSON.Invalid_JSON_Stream =>
+      when JSONParser.InvalidJSONException =>
          null;
    end Test_NoJSONError;
 
    procedure Test_QueryTooSmallError (T : in out Test) is
-      use GNATCOLL.JSON;
-      pragma Unreferenced (T);
       jsonstring : constant String :=
 "{""type"":""error"",""resultcount"":0,""results"":""Query arg too small""}";
       json : JSON_Value;
+      pragma Unreferenced (T);
    begin
-      json := Read (Strm     => jsonstring,
-                    Filename => "");
-      jsonparser.ParseJSON (json => json);
+      json := ParseJSON (Strm     => jsonstring);
+      ParseAURResponse (json);
       Assert (False, "No argument too small exception raised");
    exception
-      when jsonparser.QueryTooSmallFail =>
+      when AURParser.QueryTooSmallFail =>
          null;
    end Test_QueryTooSmallError;
 
    procedure Test_TooManyResultsError (T : in out Test) is
-      use GNATCOLL.JSON;
-      pragma Unreferenced (T);
       jsonstring : constant String :=
          "{""type"":""error"",""resultcount"":0,""" &
          "results"":""Too many package results.""}";
       json : JSON_Value;
+      pragma Unreferenced (T);
    begin
-      json := Read (Strm     => jsonstring,
-                    Filename => "");
-      jsonparser.ParseJSON (json => json);
+      json := ParseJSON (Strm     => jsonstring);
+      ParseAURResponse (json);
       Assert (False, "No argument too small exception raised");
    exception
-      when jsonparser.TooManyResultsFail =>
+      when AURParser.TooManyResultsFail =>
          null;
    end Test_TooManyResultsError;
 
    procedure Test_UnexpectedRequestType (T : in out Test) is
-      use GNATCOLL.JSON;
-      pragma Unreferenced (T);
       jsonstring : constant String :=
          "{""type"":""error"",""resultcount"":0,""" &
          "results"":""Incorrect request type specified.""}";
       json : JSON_Value;
+      pragma Unreferenced (T);
    begin
-      json := Read (Strm     => jsonstring,
-                    Filename => "");
-      jsonparser.ParseJSON (json => json);
+      json := ParseJSON (Strm     => jsonstring);
+      ParseAURResponse (json);
       Assert (False, "No unexpected request type exception raised");
    exception
-      when jsonparser.IncorrectReqFail =>
+      when AURParser.IncorrectReqFail =>
          null;
    end Test_UnexpectedRequestType;
 
    procedure Test_UnexpectedReturnType (T : in out Test) is
-      use GNATCOLL.JSON;
-      pragma Unreferenced (T);
       jsonstring : constant String :=
          "{""type"":""thisiswrong"",""resultcount"":0,""" &
          "results"":""this is not parsed.""}";
       json : JSON_Value;
+      pragma Unreferenced (T);
    begin
-      json := Read (Strm     => jsonstring,
-                    Filename => "");
-      jsonparser.ParseJSON (json => json);
+      json := ParseJSON (Strm     => jsonstring);
+      ParseAURResponse (json);
       Assert (False, "No unexpected return type exception raised");
    exception
-      when jsonparser.UnknownReturnTypeFail =>
+      when AURParser.UnknownReturnTypeFail =>
          null;
    end Test_UnexpectedReturnType;
 end JSON_Parser_Test;
