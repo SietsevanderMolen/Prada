d5ebabd5053a08ccf734fb68797cc4bd177a1e91
Sietse van der Molen <sietse@vdmolen.eu>
2013-01-29 09:06:16 +0200

    remove artifacts
    Signed-off-by: Sietse van der Molen <sietse@vdmolen.eu>

--- prada/src/prada.adb
+++ prada/src/prada.adb
@@ -5,9 +5,4 @@ procedure prada is
    results : AurReply;
 begin
    results := AurInterface.search ("aa");
-   --  Try to convert to a json object
-   --  resultlist := ParseJSON (Strm     => To_String (str));
-
-   --  Parse the json object
-   --  ParseAURResponse (json => resultlist);
 end prada;
