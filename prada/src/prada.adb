6019599f04c4ae410602163ee79f093e815113b4
Sietse van der Molen <sietse@vdmolen.eu>
2013-01-29 10:33:36 +0200

    add commandline paramater checking
    Signed-off-by: Sietse van der Molen <sietse@vdmolen.eu>

--- prada/src/prada.adb
+++ prada/src/prada.adb
@@ -1,8 +1,33 @@
-with AurReplies; use AurReplies;
-with AurInterface;
+with GNAT.Command_Line;  use GNAT.Command_Line;
+--  with AurReplies; use AurReplies;
+--  with AurInterface;
+with Ada.Text_IO;
 
 procedure prada is
-   results : AurReply;
+   --  results : AurReply;
+   procedure Display_Help;
+
+   procedure Display_Help is
+   begin
+      Ada.Text_IO.Put_Line ("usage: prada [option] [package] [package] [...]");
+      Ada.Text_IO.New_Line;
+      Ada.Text_IO.Put_Line ("   -Ss|-Ssq    - searches for package");
+      --  Ada.Text_IO.Put_Line ("   -Si         - outputs info for package");
+   end Display_Help;
 begin
-   results := AurInterface.search ("aa");
+   loop
+      case Getopt ("h -help") is
+         when 'h' => Display_Help;
+         when '-' =>
+            if Full_Switch = "-help" then
+               Display_Help;
+            end if;
+         when others => exit;
+      end case;
+   end loop;
+exception
+   when Invalid_Switch | Invalid_Parameter =>
+      Ada.Text_IO.Put_Line ("Prada: Option '" &
+                             Full_Switch & "' is not valid.");
+      --  results := AurInterface.search ("aa");
 end prada;
