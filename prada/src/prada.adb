0db562ef059afae28f3a5759e12cff4fcc768417
Sietse van der Molen <sietse@vdmolen.eu>
2013-02-20 08:08:59 +0200

    give prada it's own directory
    Signed-off-by: Sietse van der Molen <sietse@vdmolen.eu>

--- prada/src/prada.adb
+++
@@ -1,5 +0,0 @@
-procedure Prada
-is
-begin
-   null;
-end Prada;
---
+++ prada/src/prada/prada.adb
@@ -0,0 +1,8 @@
+with Ada.Text_IO;
+
+procedure Prada
+is
+   package ATIO renames Ada.Text_IO;
+begin
+   ATIO.Put_Line ("This does nothing.");
+end Prada;
