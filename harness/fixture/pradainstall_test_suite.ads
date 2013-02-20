7a0e148c6360fd882df88dc38f6815b7590f34e5
Sietse van der Molen <sietse@vdmolen.eu>
2013-02-20 10:53:36 +0200

    update aunit harness to fit new project layout
    Signed-off-by: Sietse van der Molen <sietse@vdmolen.eu>

---
+++ harness/fixture/pradainstall_test_suite.ads
@@ -0,0 +1,7 @@
+with AUnit.Test_Suites; use AUnit.Test_Suites;
+
+package PradaInstall_Test_Suite is
+
+   function Suite return Access_Test_Suite;
+
+end PradaInstall_Test_Suite;
---
+++ harness/tests/pradainstall_test.ads
@@ -0,0 +1,9 @@
+with AUnit.Test_Fixtures;
+
+package PradaInstall_Test is
+
+   type Test is new AUnit.Test_Fixtures.Test_Fixture with null record;
+
+   procedure Test_NoInputReturnCode (T : in out Test);
+   --  Test if the program gives the right exit code when no input is given
+end PradaInstall_Test;
