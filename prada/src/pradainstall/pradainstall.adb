d10fe2681e6c0b55d192e12da6da8b8f5e2237e0
Sietse van der Molen <sietse@vdmolen.eu>
2013-02-20 11:12:23 +0200

    fix some trivial warnings
    Signed-off-by: Sietse van der Molen <sietse@vdmolen.eu>

--- prada/src/pradainstall/pradainstall.adb
+++ prada/src/pradainstall/pradainstall.adb
@@ -1,13 +1,14 @@
-with Ada.Text_IO; use Ada.Text_IO;
 with GNAT.OS_Lib;
 --  with GNAT.Command_Line;  use GNAT.Command_Line;
 
 procedure PradaInstall is
+   procedure ParseCommandLine;
+
    procedure ParseCommandLine is
    begin
       null;
    end ParseCommandLine;
 begin
    ParseCommandLine;
-   GNAT.OS_Lib.OS_Exit(0);
+   GNAT.OS_Lib.OS_Exit (0);
 end PradaInstall;
