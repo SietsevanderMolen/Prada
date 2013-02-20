f688777aaec4c7b458e73b546ce6a50553dbee06
Sietse van der Molen <sietse@vdmolen.eu>
2013-02-20 10:52:34 +0200

    add some random code to test new layout
    Signed-off-by: Sietse van der Molen <sietse@vdmolen.eu>

--- prada/src/pradainstall/pradainstall.adb
+++ prada/src/pradainstall/pradainstall.adb
@@ -1,5 +1,13 @@
-procedure PradaInstall
-is
+with Ada.Text_IO; use Ada.Text_IO;
+with GNAT.OS_Lib;
+--  with GNAT.Command_Line;  use GNAT.Command_Line;
+
+procedure PradaInstall is
+   procedure ParseCommandLine is
+   begin
+      null;
+   end ParseCommandLine;
 begin
-   null;
+   ParseCommandLine;
+   GNAT.OS_Lib.OS_Exit(0);
 end PradaInstall;
